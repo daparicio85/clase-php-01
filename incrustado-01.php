434343444ec07c1bed6d24a52c2594d8f20f01d2
daparicio85 <43053643@idexperujapon.edu.pe>
2024-08-23 21:16:58 -0500

    agrege incrustado 01 y 02

---
+++ incrustado-01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Incrustado 01</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hola mundo desde php incrustado";
+        ?>
+    </h1>
+</body>
+</html>
