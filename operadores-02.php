7b846ecf725fabece07d1438504103a27ce292fd
daparicio85 <43053643@idexperujapon.edu.pe>
2024-08-23 21:36:50 -0500

    agregando operadores 1,2 y 3

---
+++ operadores-02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores 02</title>
+</head>
+<body>
+    <h1>Concatenando...</h1>
+    <?php
+        $cadena1 = "Juan";
+        $cadena2 = "Perez";
+        $cadena3 = "Ramirez";
+    ?>
+    <p>
+        <?php
+            echo $cadena1.' '.$cadena2.' '.$cadena3;
+        ?>
+    </p>
+</body>
+</html>
